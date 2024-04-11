19cb6495077f712934f147e3dd57b79d191563a5
Aizaz Sh <aizazshahid786@gmail.com>
2024-04-11 15:14:36 +0500

    will setup the initial project with Laravel livewire setup

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div>
+    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+	<h1>{{ $count }}</h1>
+ 
+    <button wire:click="increment">+</button>
+ 
+    <button wire:click="decrement">-</button>
+</div>
